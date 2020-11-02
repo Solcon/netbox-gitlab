c3bee80949e7305f3ad43e28c518c7bae2518f2d
Sander Steffann <sander@steffann.nl>
2020-11-02 22:46:11 +0100

    Cast to list to avoid caching errors

--- netbox_gitlab/hacks.py
+++ netbox_gitlab/hacks.py
@@ -10,8 +10,8 @@ from ipam.models import VLAN, VRF
 def vlans(self):
     device = self
 
-    tagged_vlan_ids = device.vc_interfaces.values_list('untagged_vlan', flat=True)
-    untagged_vlan_ids = device.vc_interfaces.values_list('tagged_vlans', flat=True)
+    tagged_vlan_ids = list(device.vc_interfaces.values_list('untagged_vlan', flat=True))
+    untagged_vlan_ids = list(device.vc_interfaces.values_list('tagged_vlans', flat=True))
 
     return VLAN.objects.filter(Q(id__in=tagged_vlan_ids) | Q(id__in=untagged_vlan_ids))
 
@@ -19,7 +19,7 @@ def vlans(self):
 def vrfs(self):
     device = self
 
-    vrf_ids = device.vc_interfaces.values_list('ip_addresses__vrf', flat=True)
+    vrf_ids = list(device.vc_interfaces.values_list('ip_addresses__vrf', flat=True))
     return VRF.objects.filter(id__in=vrf_ids)
 
 
